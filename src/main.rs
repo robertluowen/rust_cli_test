466282bb93abf5b0752cfd2182e78350b4a2cfa9
luowen <757120778@qq.com>
2024-07-03 15:11:03 +0800

    docker ci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
+
+#[get("/again")]
+async fn again() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("Starting HTTP server: go to http://localhost:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(index)
+            .service(again)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
+
